bf852a6dc33c657a83a0a1f6b4298ad3dc9ecb5f
Rellik_Pan <pan820124@gmail.com>
2023-10-06 16:03:14 +0800

    feat: 🎸 async 寄信功能
    新增使用reactive mailer作非同步的寄信

--- quarkus-dummy/sendmail-sample/src/main/java/sendgrid/MailResource.java
+++ quarkus-dummy/sendmail-sample/src/main/java/sendgrid/MailResource.java
@@ -2,12 +2,11 @@ package sendgrid;
 
 import io.quarkus.mailer.Mail;
 import io.quarkus.mailer.Mailer;
+import io.quarkus.mailer.reactive.ReactiveMailer;
 import io.smallrye.common.annotation.Blocking;
-
+import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
-
-import static java.lang.System.getenv;
 
 @Path("/mail")
 public class MailResource {
@@ -15,12 +14,21 @@ public class MailResource {
     @Inject
     Mailer mailer;
 
+    @Inject
+    ReactiveMailer reactiveMailer;
+
     @GET
     @Blocking
+    @Path("single")
     public void sendEmail(@QueryParam("address") String address) {
-        mailer.send(
-            Mail.withText(address, "Ahoy from Quarkus", "A simple email sent from a Quarkus application.")
-        );
+        mailer.send(Mail.withText(address, "Ahoy from Quarkus", "A simple email sent from a Quarkus application."));
+    }
+
+    @GET
+    @Path("/async")
+    public Uni<Void> asyncSendEmail(@QueryParam("address") String address) {
+        return reactiveMailer.send(Mail.withText(address, "A reactive email from quarkus",
+                "A simple email sent from a Quarkus application."));
     }
 
 }
