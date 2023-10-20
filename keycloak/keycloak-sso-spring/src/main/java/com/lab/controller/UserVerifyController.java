5432dcebe7ccd76b174cd53ea19ae2cf6feae733
pal2097 <pal2097@gmail.com>
2023-10-20 10:30:27 +0800

    feature: Update keycloak lab code

--- keycloak/keycloak-sso-spring/src/main/java/com/lab/controller/UserVerifyController.java
+++ keycloak/keycloak-sso-spring/src/main/java/com/lab/controller/UserVerifyController.java
@@ -1,5 +1,6 @@
 package com.lab.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/hello")
 public class UserVerifyController {
 
-    @GetMapping("/login")
+    @GetMapping("/demo")
     public String login() {
         return "auth check ok";
     }
